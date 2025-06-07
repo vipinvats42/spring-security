481fccf9bb7afff2bfdcdc9c236e2c6e5e4e89e9
Vipin <=vipinvats42@gmail.com>
2025-06-07 07:07:53 +0530

    spring security and logging changes

---
+++ src/main/java/com/example/pranshee/SpringSecuritySection1Application.java
@@ -0,0 +1,13 @@
+package com.example.pranshee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySection1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pranshee/SpringSecuritySection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pranshee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
