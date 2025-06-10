dc0c0dde7f28f1af028a09c87991ee3bca746b7e
Vipin <=vipinvats42@gmail.com>
2025-06-10 22:31:47 +0530

    diable form login and httpbasic dible test

--- src/main/java/com/example/pranshee/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/pranshee/config/ProjectSecurityConfig.java
@@ -14,7 +14,13 @@ public class ProjectSecurityConfig {
 	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		//http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+		//http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards").authenticated()
+				.requestMatchers("/myNotice", "/myContact","/error").permitAll());
+		//http.formLogin(httpSecurityFormLoginConfrigurer-> httpSecurityFormLoginConfrigurer.disable());
+		//http.httpBasic(httpBasicConfig -> httpBasicConfig.disable());
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
